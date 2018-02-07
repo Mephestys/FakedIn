a53cbfebbcdf6ae0823c2b5ee2e4e255d1710ce5
Mephestys <mephestys@gmail.com>
2018-02-07 16:08:13 -0700

    Created dummy data for import (not yet utilized).

---
+++ src/dummydata.js
@@ -0,0 +1,17 @@
+const dummyData = [
+  {
+    avatar: "",
+    name: "Fakename McNotrealguy",
+    title: "Nonexistant being",
+  },
+  {
+    avatar: "",
+    name: "Alsonot Realperson",
+    title: "Madeup at Fakebusiness",
+  },
+  {
+    avatar: "",
+    name: "Badat Comingupwithnames",
+    title: "Professional void of consciousness.",
+  },
+]
