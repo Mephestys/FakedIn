b96906486482dd910c8fa93550777acf51bcf7e2
Mephestys <mephestys@gmail.com>
2018-02-07 18:51:23 -0700

    Beginning styling on ConnectionContainers.

--- src/components/ConnectionContainer/ConnectionContainer.js
+++ src/components/ConnectionContainer/ConnectionContainer.js
@@ -1,20 +1,22 @@
 import React from "react";
 
+import "./ConnectionContainer.css";
+
 const ConnectionContainer = props => {
   const connection = props.connection;
   
   return (
-    <div>
+    <div className="ConnectionContainer">
       <div>
-        <img src={connection.avatar} width="64" height="64" alt="Avatar failed to load." />
+        <img className="ConnectionContainer Avatar" src={connection.avatar} width="64" height="64" alt="Avatar failed to load." />
       </div>
-      <div>
+      <div className="ConnectionContainer ConnectionName">
         {connection.name}
       </div>
-      <div>
+      <div className="ConnectionContainer ConnectionTitle">
         {connection.title}
       </div>
-      <div>
+      <div className="ConnectionContainer ConnectButton">
         <button type="submit">Connect</button>
       </div>
     </div>
